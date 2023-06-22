3689207f4f38a6eae1a6e6bc63cf7e8c28e5776f
Arif Ramadhani <62203067+arifrmdhni@users.noreply.github.com>
2023-06-22 11:35:48 +0700

    Update Commitment.js

--- src/Pages/Commitment.js
+++ src/Pages/Commitment.js
@@ -1,12 +1,10 @@
 import NavigationBar from "../Components/NavigationBar"
 const Commitment = () => {
   return (
-    <>
-    <div className="card custom-card">
     
+    <div className="card custom-card">
+
     </div>
-    <NavigationBar />
-    </>
   )
 }
 export default Commitment
